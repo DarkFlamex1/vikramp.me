bf78164a240f38834a3e9593f5583c4bd6f05924
Vikram Peddinti <vikram.peddinti@gmail.com>
2020-02-23 12:03:54 -0800

    Initial construction page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-red': '#EA7F67',
+        'custom-purple': '#E3D0D8',
+        'dusty-rose': '#BD9B96',
+        'custom-darkpurple': '#543751',
+        'custom-skyblue':'#84ADD8',
+       },
+    },
   },
   variants: {},
   plugins: [],
